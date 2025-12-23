a83df2180ff62d6f5635446a0dfa23fd5d9821e7
mcibula <mirkocibula@gmail.com>
2025-12-24 00:36:48 +0100

    Add implementations of abstract scenes and rigid objects

---
+++ scene.py
@@ -0,0 +1,96 @@
+import numpy as np
+import torch
+
+from processors.segmenter import Segmenter, TrackRecord
+from utils.image import is_duplicate
+
+
+class RigidObject:
+    def __init__(self, obj_id: int) -> None:
+        self.obj_id = obj_id
+
+        self.x: float | None = None
+        self.y: float | None = None
+        self.z: float | None = None
+        self.rx: float | None = None
+        self.ry: float | None = None
+        self.rz: float | None = None
+
+        self._masks: list[np.ndarray] = []
+        self._snapshots: list[np.ndarray] = []
+
+    @property
+    def pose_6d(self) -> tuple[float, float, float, float, float, float]:
+        return self.x, self.y, self.z, self.rx, self.ry, self.rz
+
+    @property
+    def num_snapshots(self) -> int:
+        return len(self._snapshots)
+
+    @property
+    def masks(self) -> list[np.ndarray]:
+        return self._masks
+
+    @property
+    def snapshots(self) -> list[np.ndarray]:
+        return self._snapshots
+
+    def set_pose_6d(
+            self,
+            x: float, y: float, z: float,
+            rx: float, ry: float, rz: float
+    ) -> None:
+        (
+            self.x, self.y, self.z,
+            self.rx, self.ry, self.rz
+        ) = (
+            x, y, z,
+            rx, ry, rz
+        )
+
+    def register_visuals(self, snapshots: list[np.ndarray], masks: list[np.ndarray]) -> None:
+        if len(snapshots) != len(masks) or not snapshots:
+            raise ValueError
+
+        if not self._snapshots:
+            self._snapshots.append(snapshots[0])
+            self._masks.append(masks[0])
+
+        for snapshot, mask in zip(snapshots[1:], masks[1:]):
+            if is_duplicate(snapshot, self._snapshots[-1], thresh=27.0):
+                continue
+
+            self._snapshots.append(snapshot)
+            self._masks.append(mask)
+
+
+class Scene:
+    def __init__(self, segmenter: Segmenter) -> None:
+        self.device: str = 'cuda' if torch.cuda.is_available() else 'cpu'
+        self.segmenter = segmenter
+
+        self.frame_count: int = 0
+        self.objects: dict[int, RigidObject] = {}
+
+    @property
+    def num_objects(self) -> int:
+        return len(self.objects)
+
+    def read_frame(self, frame: np.ndarray) -> None:
+        self.frame_count += 1
+
+        record: dict[int, TrackRecord] = self.segmenter.track(
+            src=frame,
+            persist=True,
+            stream=False
+        )
+
+        for obj_id, obj_record in record.items():
+            if obj_id not in self.objects:
+                self.objects[obj_id] = RigidObject(obj_id)
+
+            obj: RigidObject = self.objects[obj_id]
+            obj.register_visuals(
+                snapshots=obj_record.snapshots,
+                masks=obj_record.masks
+            )
