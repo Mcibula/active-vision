636cbd7fbb424a3dec36b5da3ade34bfe30cca3f
mcibula <mirkocibula@gmail.com>
2026-01-14 02:20:03 +0100

    Extract the testing camera snap tool

---
+++ scripts/snap.py
@@ -0,0 +1,54 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pyrealsense2 as rs
+
+from camera import RealsenseCamera, Stream, Streams
+
+camera = RealsenseCamera(
+    Streams([
+        Stream(
+            name='color',
+            stype=rs.stream.color,
+            w_res=1920,
+            h_res=1080,
+            dtype=rs.format.rgb8,
+            fps=30,
+            extractor=lambda frames: frames.get_color_frame(),
+            roi=(504, 0, 1038, 1080)
+        ),
+        Stream(
+            name='depth',
+            stype=rs.stream.depth,
+            w_res=1280,
+            h_res=720,
+            dtype=rs.format.z16,
+            fps=30,
+            extractor=lambda frames: frames.get_depth_frame(),
+            # colorizer=rs.colorizer(color_scheme=0),
+            roi=(411, 106, 478, 491)
+        )
+    ])
+)
+
+try:
+    camera.start_streaming()
+    camera.warmup(t=4.0)
+
+    frame: np.ndarray | None = None
+    while frame is None:
+        frame = camera.get_frame(['color', 'depth'])
+
+    rgb_frame, depth_frame = frame
+
+finally:
+    camera.stop_streaming()
+
+fig, (ax1, ax2) = plt.subplots(ncols=2, figsize=(12, 6))
+ax1.imshow(rgb_frame)
+ax2.imshow(depth_frame)
+
+fig.tight_layout()
+plt.show()
+
+# np.save('../experiments/test_imgs/rgb_frame.npy', rgb_frame)
+# np.save('../experiments/test_imgs/depth_frame.npy', depth_frame)
