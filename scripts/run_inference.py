f4feb8134dfb8cf842d928b185fb949fec3c841f
mcibula <mirkocibula@gmail.com>
2026-01-23 23:30:13 +0100

    Reconfigure the inference executor

--- scripts/run_inference.py
+++ scripts/run_inference.py
@@ -1,16 +1,29 @@
 from camera import RealsenseCamera
 from camera.profiles import FHD_RGB, HD_DEPTH_ALIGN
 from core import PipelineController, Scene
-from processors import Segmenter
+from processors import PoseEstimator, Segmenter
 
 
 def main() -> Scene:
     camera = RealsenseCamera([FHD_RGB, HD_DEPTH_ALIGN])
+
     segmenter = Segmenter(
         engine='yoloe',
         weights='../models/yolo/yoloe-11l-seg-pf.pt'
     )
-    scene = Scene(segmenter=segmenter)
+    pose_estimator = PoseEstimator(
+        camera_intrinsics=camera['color'].intrinsics,
+        detector_weights='L-C4-v2',
+        descriptor_weights='G-upright',
+        score_thresh=0.5,
+        match_thresh=6,
+        estimation_rng=(0.1, 5.0)
+    )
+
+    scene = Scene(
+        segmenter=segmenter,
+        pose_estimator=pose_estimator
+    )
 
     controller = PipelineController(
         camera=camera,
