b4590557d048034a9bebd2a373d4efd729d7dab2
mcibula <mirkocibula@gmail.com>
2026-01-24 02:29:10 +0100

    Implement the `timer` debug utility

--- src/utils/misc.py
+++ src/utils/misc.py
@@ -1,6 +1,13 @@
-from typing import Literal
+from __future__ import annotations
+
+import time
+from typing import TYPE_CHECKING
 
 import torch
+
+if TYPE_CHECKING:
+    from types import TracebackType
+
 
 def infer_device() -> torch.device:
     return torch.device(
@@ -10,5 +17,21 @@ def infer_device() -> torch.device:
     )
 
 
-def infer_device() -> Literal['cpu', 'cuda']:
-    return 'cuda' if torch.cuda.is_available() else 'cpu'
+class timer:
+    def __init__(self, name: str = '') -> None:
+        self.name: str = name
+
+    def __enter__(self) -> timer:
+        self.start: float = time.perf_counter()
+        return self
+
+    def __exit__(
+            self,
+            exc_type: type[BaseException] | None,
+            exc_val: BaseException | None,
+            exc_tb: TracebackType
+    ) -> None:
+        self.time = time.perf_counter() - self.start
+        self.readout = f'Operation{" " + self.name if self.name else ""}: {self.time * 1000:.2f} ms'
+
+        print(self.readout)
