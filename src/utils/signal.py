c27adcb2b3ecd8b1e4dd635e693a1a8e57cfd552
mcibula <mirkocibula@gmail.com>
2026-04-01 01:34:56 +0200

    docs: add docs for `OneEuroFilter`

--- src/utils/signal.py
+++ src/utils/signal.py
@@ -7,7 +7,25 @@ import time
 
 
 class OneEuroFilter:
+    """
+    Simple Online 1€ Filter implementation for filtering trajectory signals.
+
+    Adapted from:
+        https://github.com/jaantollander/OneEuroFilter
+        and https://github.com/casiez/OneEuroFilter
+
+    See also: https://jaantollander.com/post/noise-filtering-using-one-euro-filter/
+    """
+
     def __init__(self, min_cutoff: float = 1.0, beta: float = 0.0, d_cutoff: float = 1.0) -> None:
+        """
+        Initialize a 1€ Filter instance
+
+        :param min_cutoff: Minimum cutoff frequency in Hz; lower values remove more jitter
+        :param beta: Speed coefficient
+        :param d_cutoff: Derivative cutoff frequency in Hz
+        """
+
         # Parameters
         self.min_cutoff: float = min_cutoff
         self.beta: float = beta
@@ -19,6 +37,14 @@ class OneEuroFilter:
         self.t_prev: float | None = None
 
     def __call__(self, x: float, t: float | None = None) -> float:
+        """
+        Compute the filtered signal for the given point `x` at time `t`
+
+        :param x: Signal point value
+        :param t: Timestamp in seconds
+        :return: Filtered signal point value
+        """
+
         if t is None:
             t = time.time()
 
@@ -28,6 +54,10 @@ class OneEuroFilter:
             self.t_prev = t
 
             return x
+
+        self.x_prev: float
+        self.dx_prev: float
+        self.t_prev: float
 
         dt = t - self.t_prev
 
@@ -59,6 +89,17 @@ class OneEuroFilter:
 
     @staticmethod
     def _exp_smooth(x: float, x_prev: float, dt: float, cutoff: float) -> float:
+        """
+        Perform exponential smoothing on `x`
+
+        :param x: Current point value
+        :param x_prev: Point value in the previous time step
+        :param dt: Time difference between `x` and `x_prev` in seconds
+        :param cutoff: Cutoff frequency in Hz
+        :return: Smoothed `x`
+        """
+
+        # Compute the smoothing factor
         tau = 1.0 / (2.0 * math.pi * cutoff)
         alpha = 1.0 / (1.0 + tau / dt)
 
